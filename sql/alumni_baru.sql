9abbddc8efab663d6212c20300c01892a51cc661
Redi Vanhar <vanhar_redi@yahoo.co.id>
2012-10-14 00:32:53 +0700

    sql baru ni

--- sql/alumni_baru.sql
+++ sql/alumni_baru.sql
@@ -1,33 +1,26 @@
--- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Oct 11, 2012 at 02:20 PM
--- Server version: 5.5.24-log
--- PHP Version: 5.3.13
+/*
+SQLyog Enterprise - MySQL GUI v7.14 
+MySQL - 5.5.8 : Database - alumni_baru
+*********************************************************************
+*/
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `alumni_baru`
---
+/*!40101 SET SQL_MODE=''*/;
 
--- --------------------------------------------------------
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
---
--- Table structure for table `data_alumni`
---
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`alumni_baru` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-CREATE TABLE IF NOT EXISTS `data_alumni` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+USE `alumni_baru`;
+
+/*Table structure for table `data_alumni` */
+
+DROP TABLE IF EXISTS `data_alumni`;
+
+CREATE TABLE `data_alumni` (
+  `id_alumni` int(11) NOT NULL AUTO_INCREMENT,
   `id_user` int(11) NOT NULL,
   `nim` varchar(15) NOT NULL,
   `nama` varchar(255) NOT NULL,
@@ -46,52 +39,54 @@ CREATE TABLE IF NOT EXISTS `data_alumni` (
   `alamat_ot` text NOT NULL,
   `telepon_ot` varchar(20) NOT NULL,
   `ponsel_ot` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id_alumni`),
   UNIQUE KEY `id_user` (`id_user`),
-  KEY `id_jurusan` (`id_jurusan`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  KEY `id_jurusan` (`id_jurusan`),
+  CONSTRAINT `data_alumni_ibfk_2` FOREIGN KEY (`id_jurusan`) REFERENCES `data_jurusan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `data_alumni`
---
+/*Data for the table `data_alumni` */
 
-INSERT INTO `data_alumni` (`id`, `id_user`, `nim`, `nama`, `id_jurusan`, `ttl`, `tgl_daftar`, `tgl_yudisium`, `tgl_wisuda`, `lama_studi`, `ipk`, `alamat`, `email`, `telepon`, `ponsel`, `nama_ot`, `alamat_ot`, `telepon_ot`, `ponsel_ot`) VALUES
-(1, 1, '59081003041', 'Nurimansyah Rifwan', 2, 'Jakarta, 03 April 1989', '2012-10-10', '2012-12-01', '2012-12-02', '5 Tahun', 3.62, 'Jl. Pondok Bambu Asri Timur V No. 16', 'nurimansyah.rifwan@gmail.com', '+62711', '', 'Deswita', 'Jl. Amal No. 34', '+62', '');
+insert  into `data_alumni`(`id_alumni`,`id_user`,`nim`,`nama`,`id_jurusan`,`ttl`,`tgl_daftar`,`tgl_yudisium`,`tgl_wisuda`,`lama_studi`,`ipk`,`alamat`,`email`,`telepon`,`ponsel`,`nama_ot`,`alamat_ot`,`telepon_ot`,`ponsel_ot`) values (1,1,'59081003041','Nurimansyah Rifwan',2,'Jakarta, 03 April 1989','2012-10-10','2012-12-01','2012-12-02','5 Tahun',3.62,'Jl. Pondok Bambu Asri Timur V No. 16','nurimansyah.rifwan@gmail.com','+62711','','Deswita','Jl. Amal No. 34','+62',''),(2,4,'09071003051','Nurimansyah Rifwan',2,'Jakarta, 03 April 1989','2012-10-13','2012-01-01','2012-01-02','4 Tahun',4,'Jladkasd','nurimansyah.rifwan@gmail.com','+62711','+62','Deswita','adadasdf','+62','+62');
 
--- --------------------------------------------------------
+/*Table structure for table `data_jurusan` */
 
---
--- Table structure for table `data_jurusan`
---
+DROP TABLE IF EXISTS `data_jurusan`;
 
-CREATE TABLE IF NOT EXISTS `data_jurusan` (
+CREATE TABLE `data_jurusan` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(255) NOT NULL,
   `prodi` enum('D3','S1') NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `data_jurusan`
---
+/*Data for the table `data_jurusan` */
 
-INSERT INTO `data_jurusan` (`id`, `nama`, `prodi`) VALUES
-(1, 'Sistem Informasi Reguler', 'S1'),
-(2, 'Sistem Informasi Bilingual', 'S1'),
-(3, 'Sistem Komputer', 'S1'),
-(4, 'Teknik Informatika Reguler', 'S1'),
-(5, 'Teknik Informatika Bilingual', 'S1'),
-(6, 'Komputerisasi Akuntansi', 'D3'),
-(7, 'Manajemen Informatika', 'D3'),
-(8, 'Teknik Komputer', 'D3');
+insert  into `data_jurusan`(`id`,`nama`,`prodi`) values (1,'Sistem Informasi Reguler','S1'),(2,'Sistem Informasi Bilingual','S1'),(3,'Sistem Komputer','S1'),(4,'Teknik Informatika Reguler','S1'),(5,'Teknik Informatika Bilingual','S1'),(6,'Komputerisasi Akuntansi','D3'),(7,'Manajemen Informatika','D3'),(8,'Teknik Komputer','D3');
 
--- --------------------------------------------------------
+/*Table structure for table `data_pa` */
 
---
--- Table structure for table `data_pekerjaan`
---
+DROP TABLE IF EXISTS `data_pa`;
 
-CREATE TABLE IF NOT EXISTS `data_pekerjaan` (
+CREATE TABLE `data_pa` (
+  `id_pa` int(20) NOT NULL AUTO_INCREMENT,
+  `id_user` int(20) DEFAULT NULL,
+  `nama_pa` varchar(40) DEFAULT NULL,
+  `alamat_pa` varchar(40) DEFAULT NULL,
+  `bidang_usaha` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id_pa`),
+  KEY `FK_data_pa_user` (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `data_pa` */
+
+insert  into `data_pa`(`id_pa`,`id_user`,`nama_pa`,`alamat_pa`,`bidang_usaha`) values (1,NULL,'pertamana','palembang','bongkar minyak');
+
+/*Table structure for table `data_pekerjaan` */
+
+DROP TABLE IF EXISTS `data_pekerjaan`;
+
+CREATE TABLE `data_pekerjaan` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_user` int(11) NOT NULL,
   `tgl_daftar` date NOT NULL,
@@ -100,57 +95,30 @@ CREATE TABLE IF NOT EXISTS `data_pekerjaan` (
   `alamat` text NOT NULL,
   `jabatan` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `id_user` (`id_user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `data_pekerjaan_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `data_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `data_pekerjaan`
---
+/*Data for the table `data_pekerjaan` */
 
-INSERT INTO `data_pekerjaan` (`id`, `id_user`, `tgl_daftar`, `tempat`, `status`, `alamat`, `jabatan`) VALUES
-(1, 1, '2012-10-10', 'Universitas Sriwjaya', 'Aktif', 'Jl. Srijaya Negara', 'CEO');
+insert  into `data_pekerjaan`(`id`,`id_user`,`tgl_daftar`,`tempat`,`status`,`alamat`,`jabatan`) values (1,1,'2012-10-10','Universitas Sriwjaya','Aktif','Jl. Srijaya Negara','CEO');
 
--- --------------------------------------------------------
+/*Table structure for table `data_user` */
 
---
--- Table structure for table `data_user`
---
+DROP TABLE IF EXISTS `data_user`;
 
-CREATE TABLE IF NOT EXISTS `data_user` (
+CREATE TABLE `data_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(32) NOT NULL,
   `password` varchar(32) NOT NULL,
   `level` enum('alumni','pengguna','admin') NOT NULL,
   `status` enum('0','1') NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `data_user`
---
+/*Data for the table `data_user` */
 
-INSERT INTO `data_user` (`id`, `username`, `password`, `level`, `status`) VALUES
-(1, '59081003041', '12345', 'alumni', '1'),
-(2, 'admin', 'admin', 'admin', '1'),
-(3, '59081003043', 'qwerty', 'alumni', '0');
+insert  into `data_user`(`id`,`username`,`password`,`level`,`status`) values (1,'59081003041','12345','alumni','1'),(2,'admin','admin','admin','1'),(3,'59081003043','qwerty','alumni','0'),(4,'09071003051','basing','alumni','1'),(5,'pertamina','pertamina','pengguna','1');
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `data_alumni`
---
-ALTER TABLE `data_alumni`
-  ADD CONSTRAINT `data_alumni_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `data_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `data_alumni_ibfk_2` FOREIGN KEY (`id_jurusan`) REFERENCES `data_jurusan` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `data_pekerjaan`
---
-ALTER TABLE `data_pekerjaan`
-  ADD CONSTRAINT `data_pekerjaan_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `data_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
